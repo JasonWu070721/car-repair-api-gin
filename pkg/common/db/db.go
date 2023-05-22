e6ef8871317a1073f7a3e6a7b8c90227d22c6bce
Jason Wu <jasonwu070721@gmail.com>
2023-05-22 23:15:14 +0800

    Add Redis cache

--- pkg/common/db/db.go
+++ pkg/common/db/db.go
@@ -6,7 +6,13 @@ import (
 	"car_repair_api_go/pkg/common/models"
 
 	"gorm.io/driver/postgres"
+
 	"gorm.io/gorm"
+
+    "context"
+    "github.com/redis/go-redis/v9"
+    "fmt"
+
 )
 
 func Init(url string) *gorm.DB {
@@ -17,6 +23,28 @@ func Init(url string) *gorm.DB {
     }
 
     db.AutoMigrate(&models.User{})
+    SetApiVersionCache()
 
     return db
+}
+
+var ctx = context.Background()
+
+func SetApiVersionCache() {
+    rdb := redis.NewClient(&redis.Options{
+        Addr:     "redis:6379",
+        Password: "",
+        DB:       0,
+    })
+
+    err := rdb.Set(ctx, "api_verion", "1.0.0", 0).Err()
+    if err != nil {
+        panic(err)
+    }
+
+    val, err := rdb.Get(ctx, "api_verion").Result()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("api_verion", val)
 }
